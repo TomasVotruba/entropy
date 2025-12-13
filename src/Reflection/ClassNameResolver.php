edcdd3abd1e922e444110ca9bb3159ea6eae467c
Tomas Votruba <tomas.vot@gmail.com>
2025-12-13 22:27:15 +0100

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! misc

--- src/Reflection/ClassNameResolver.php
+++ src/Reflection/ClassNameResolver.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Entropy\Reflection;
 
+use Entropy\Attributes\RelatedTest;
+
+#[RelatedTest(\Entropy\Reflection\ClassNameResolver::class)]
 final class ClassNameResolver
 {
     /**
