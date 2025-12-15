6be4d473cc89b47bf3504f87eeea1d7a26001e52
Tomas Votruba <tomas.vot@gmail.com>
2025-12-15 11:37:33 +0100

    fixup! fixup! wip

--- tests/Console/ConsoleApplicationTest.php
+++ tests/Console/ConsoleApplicationTest.php
@@ -13,19 +13,12 @@ final class ConsoleApplicationTest extends TestCase
 {
     public function test(): void
     {
-        //        $inputParser = new InputParser();
-        //        $helpPrinter = new HelpPrinter();
-
         $container = new Container();
         $container->service(CommandRegistry::class, function () {
             return new CommandRegistry([]);
         });
 
-        //        $commandRegistry = new CommandRegistry([]);
-
         $consoleApplication = $container->make(ConsoleApplication::class);
-        //        $consoleApplication = new ConsoleApplication($helpPrinter, $inputParser, $commandRegistry);
-
         $consoleApplication->run([]);
     }
 }
