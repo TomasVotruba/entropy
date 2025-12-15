5184f07ce3226c17726bbb0f1201b6d3117c4eba
Tomas Votruba <tomas.vot@gmail.com>
2025-12-15 11:56:36 +0100

    misc

---
+++ tests/Console/bin/some-app.php
@@ -0,0 +1,19 @@
+<?php
+
+// simple manual test to run console app
+
+require __DIR__ . '/../../../vendor/autoload.php';
+
+use Entropy\Console\CommandRegistry;
+use Entropy\Container\Container;
+use Entropy\Tests\Console\Fixture\SimpleCommand;
+
+$container = new Container();
+$container->service(CommandRegistry::class, function (Container $container): CommandRegistry {
+    $someCommand = $container->make(SimpleCommand::class);
+
+    return new CommandRegistry([$someCommand]);
+});
+
+$consoleApplication = $container->make(\Entropy\Console\ConsoleApplication::class);
+$consoleApplication->run($argv);
