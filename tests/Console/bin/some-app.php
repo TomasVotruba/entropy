116dc9f4b8b7cccc76cff90f0093be9dfd97f150
Tomas Votruba <tomas.vot@gmail.com>
2025-12-15 12:00:59 +0100

    fixup! fixup! fixup! misc

--- tests/Console/bin/some-app.php
+++ tests/Console/bin/some-app.php
@@ -5,15 +5,18 @@
 require __DIR__ . '/../../../vendor/autoload.php';
 
 use Entropy\Console\CommandRegistry;
+use Entropy\Console\ConsoleApplication;
 use Entropy\Container\Container;
 use Entropy\Tests\Console\Fixture\SimpleCommand;
 
 $container = new Container(getcwd());
-//$container->service(CommandRegistry::class, function (Container $container): CommandRegistry {
-//    $someCommand = $container->make(SimpleCommand::class);
-//
-//    return new CommandRegistry([$someCommand]);
-//});
 
-$consoleApplication = $container->make(\Entropy\Console\ConsoleApplication::class);
+// @todo this should not be need, as another useless manual labour
+$container->service(CommandRegistry::class, function (Container $container): CommandRegistry {
+    $someCommand = $container->make(SimpleCommand::class);
+
+    return new CommandRegistry([$someCommand]);
+});
+
+$consoleApplication = $container->make(ConsoleApplication::class);
 $consoleApplication->run($argv);
