13fccc42ba75b8bc67403e00f1b84ce62e9040cc
Tomas Votruba <tomas.vot@gmail.com>
2025-12-17 12:33:25 +0100

    wip

---
+++ src/Console/Mapper/CommandRunParametersMapper.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Entropy\Console\Mapper;
+
+use Entropy\Console\Contract\CommandInterface;
+
+final class CommandRunParametersMapper
+{
+    public function map(CommandInterface $command): array
+    {
+
+    }
+}
---
+++ src/Console/ValueObject/ArgumentsAndOptions.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Entropy\Console\ValueObject;
+
+use Webmozart\Assert\Assert;
+
+final class ArgumentsAndOptions
+{
+    /**
+     * @param Argument[] $arguments
+     * @param Option[] $options
+     */
+    public function __construct(
+        private array $arguments,
+        private array $options
+    ) {
+        Assert::allIsInstanceOf($arguments, Argument::class);
+        Assert::allIsInstanceOf($options, Option::class);
+    }
+
+    /**
+     * @return Argument[]
+     */
+    public function getArguments(): array
+    {
+        return $this->arguments;
+    }
+
+    /**
+     * @return Option[]
+     */
+    public function getOptions(): array
+    {
+        return $this->options;
+    }
+}
