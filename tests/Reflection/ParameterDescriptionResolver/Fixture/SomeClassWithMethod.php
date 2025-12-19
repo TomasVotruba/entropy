e94dfaad65363f9c7f33cd8005ec30ca076b035c
Tomas Votruba <tomas.vot@gmail.com>
2025-12-19 17:13:15 +0100

    fixup! fixup! fixup! fixup! fixup! fixup! default flags

--- tests/Reflection/ParameterDescriptionResolver/Fixture/SomeClassWithMethod.php
+++ tests/Reflection/ParameterDescriptionResolver/Fixture/SomeClassWithMethod.php
@@ -5,10 +5,11 @@ namespace Entropy\Tests\Reflection\ParameterDescriptionResolver\Fixture;
 final class SomeClassWithMethod
 {
     /**
+     * @param string[] $paths
      * @param int $isEnabled Description of the option
      * @param bool $showChanges Show changes, do not apply them.
      */
-    public function someMethod(int $isEnabled, bool $showChanges)
+    public function someMethod(array $paths, int $isEnabled, bool $showChanges)
     {
     }
 }
