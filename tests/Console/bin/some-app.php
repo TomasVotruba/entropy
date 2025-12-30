25fc1f7b11e58fe1522bd62b42b97c463983d44b
Tomas Votruba <tomas.vot@gmail.com>
2025-12-30 15:56:05 +0100

    wip

--- tests/Console/bin/some-app.php
+++ tests/Console/bin/some-app.php
@@ -5,16 +5,10 @@ declare(strict_types=1);
 require __DIR__ . '/../../../vendor/autoload.php';
 
 use Entropy\Console\ConsoleApplication;
-use Entropy\Console\Output\OutputPrinter;
 use Entropy\Container\Container;
-use Entropy\Tests\Console\ConsoleApplication\Fixture\SimpleCommand;
 
 $container = new Container();
-
-$container->service(
-    SimpleCommand::class,
-    fn (Container $container): SimpleCommand => new SimpleCommand($container->make(OutputPrinter::class))
-);
+$container->autodiscover(__DIR__ . '/../ConsoleApplication/Fixture');
 
 $consoleApplication = $container->make(ConsoleApplication::class);
 $consoleApplication->run($argv);
