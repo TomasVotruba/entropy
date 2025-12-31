f1fdc0eae676a528570206733cb572ebb204c461
Tomas Votruba <tomas.vot@gmail.com>
2025-12-31 10:19:37 +0100

    wip

--- tests/Reflection/UseStatementsResolver/Fixture/SomeClass.php
+++ tests/Reflection/UseStatementsResolver/Fixture/SomeClass.php
@@ -5,7 +5,7 @@ namespace Entropy\Tests\Reflection\UseStatementsResolver\Fixture;
 use Entropy\Tests\Reflection\UseStatementsResolver\Fixture\Nested\AnotherNestedClass;
 use Entropy\Tests\Reflection\UseStatementsResolver\Fixture\Nested\EventMore\Nested;
 
-class SomeClass
+final class SomeClass
 {
     public function run(Nested $nested): AnotherNestedClass
     {
