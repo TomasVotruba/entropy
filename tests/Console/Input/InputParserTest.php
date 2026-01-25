9af81f99466b2a5e523c804ed05ccdacb29df8fe
Krzysztof Gawron <156588772+k-gawron@users.noreply.github.com>
2026-01-25 15:57:51 +0100

    Add tests for multiple option values support
    Test that repeated options are accumulated into arrays

--- tests/Console/Input/InputParserTest.php
+++ tests/Console/Input/InputParserTest.php
@@ -19,4 +19,63 @@ final class InputParserTest extends TestCase
         $this->assertSame(['src'], $cliRequest->getArguments());
         $this->assertSame([], $cliRequest->getOptions());
     }
+
+    public function testMultipleValuesForSameOption(): void
+    {
+        $inputParser = new InputParser();
+
+        $cliRequest = $inputParser->parse([
+            'bin/jack',
+            'breakpoint',
+            '--ignore=symfony/',
+            '--ignore=doctrine/',
+            '--ignore=phpunit/',
+        ]);
+
+        $this->assertSame('breakpoint', $cliRequest->getCommandName());
+        $this->assertSame([], $cliRequest->getArguments());
+        $this->assertSame([
+            'ignore' => ['symfony/', 'doctrine/', 'phpunit/'],
+        ], $cliRequest->getOptions());
+    }
+
+    public function testSingleOptionValueRemainsAsScalar(): void
+    {
+        $inputParser = new InputParser();
+
+        $cliRequest = $inputParser->parse([
+            'bin/jack',
+            'breakpoint',
+            '--ignore=symfony/',
+            '--dev',
+        ]);
+
+        $this->assertSame('breakpoint', $cliRequest->getCommandName());
+        $this->assertSame([], $cliRequest->getArguments());
+        $this->assertSame([
+            'ignore' => 'symfony/',
+            'dev' => true,
+        ], $cliRequest->getOptions());
+    }
+
+    public function testMultipleOptionsWithMixedFormats(): void
+    {
+        $inputParser = new InputParser();
+
+        $cliRequest = $inputParser->parse([
+            'bin/jack',
+            'process',
+            '--path=src',
+            '--path=tests',
+            '--dev',
+            'extra-arg',
+        ]);
+
+        $this->assertSame('process', $cliRequest->getCommandName());
+        $this->assertSame(['extra-arg'], $cliRequest->getArguments());
+        $this->assertSame([
+            'path' => ['src', 'tests'],
+            'dev' => true,
+        ], $cliRequest->getOptions());
+    }
 }
