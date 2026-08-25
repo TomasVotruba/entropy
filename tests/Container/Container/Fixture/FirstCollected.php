fa763e4fc4271658e9a7e1f41279ed2a4911765c
Tomas Votruba <tomas.vot@gmail.com>
2026-08-25 16:38:26 +0100

    Restore afterResolving() post-build hook with deferred draining (#17)

--- tests/Container/Container/Fixture/FirstCollected.php
+++ tests/Container/Container/Fixture/FirstCollected.php
@@ -6,4 +6,15 @@ namespace Entropy\Tests\Container\Container\Fixture;
 
 final class FirstCollected implements CollectedInterface
 {
+    private ?CollectedAggregate $collectedAggregate = null;
+
+    public function setCollectedAggregate(CollectedAggregate $collectedAggregate): void
+    {
+        $this->collectedAggregate = $collectedAggregate;
+    }
+
+    public function getCollectedAggregate(): ?CollectedAggregate
+    {
+        return $this->collectedAggregate;
+    }
 }
