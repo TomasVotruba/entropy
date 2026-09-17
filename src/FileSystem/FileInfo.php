e95d3ca5132099360b21bfe95e9f5fd99d2fe5fe
Tomas Votruba <tomas.vot@gmail.com>
2026-09-17 23:43:47 +0200

    Add generic FileFinder::find() with relative-path aware FileInfo

---
+++ src/FileSystem/FileInfo.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Entropy\FileSystem;
+
+use Entropy\Utils\FileSystem;
+use SplFileInfo;
+
+final class FileInfo extends SplFileInfo
+{
+    public function __construct(
+        string $filePath,
+        private readonly string $relativePath = '',
+        private readonly string $relativePathname = '',
+    ) {
+        parent::__construct($filePath);
+    }
+
+    /**
+     * @api
+     */
+    public function getContents(): string
+    {
+        return FileSystem::read($this->getPathname());
+    }
+
+    /**
+     * @api
+     */
+    public function getRelativePath(): string
+    {
+        return $this->relativePath;
+    }
+
+    /**
+     * @api
+     */
+    public function getRelativePathname(): string
+    {
+        return $this->relativePathname;
+    }
+}
