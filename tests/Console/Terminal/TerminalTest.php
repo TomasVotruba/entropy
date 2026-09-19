5aaf090d922dcb0a28e44d27eac73085d6304baf
Vladyslav M. <vladyslav.mudrichenko@ukr.net>
2026-09-19 11:31:01 +0300

    fix(console): avoid stty execution on Windows

--- tests/Console/Terminal/TerminalTest.php
+++ tests/Console/Terminal/TerminalTest.php
@@ -38,4 +38,42 @@ final class TerminalTest extends TestCase
 
         $this->assertSame(120, Terminal::getWidth());
     }
+
+    public function testGetWidthDoesNotWriteToStderrOnWindows(): void
+    {
+        if (\PHP_OS_FAMILY !== 'Windows') {
+            self::markTestSkipped('This test requires Windows.');
+        }
+
+        $process = proc_open(
+            [
+                PHP_BINARY,
+                '-r',
+                "require 'vendor/autoload.php'; putenv('COLUMNS'); echo \\Entropy\\Console\\Terminal\\Terminal::getWidth();",
+            ],
+            [
+                0 => ['pipe', 'r'],
+                1 => ['pipe', 'w'],
+                2 => ['pipe', 'w'],
+            ],
+            $pipes,
+            dirname(__DIR__, 3)
+        );
+
+        self::assertIsResource($process);
+
+        fclose($pipes[0]);
+
+        $stdout = stream_get_contents($pipes[1]);
+        $stderr = stream_get_contents($pipes[2]);
+
+        fclose($pipes[1]);
+        fclose($pipes[2]);
+
+        $exitCode = proc_close($process);
+
+        self::assertSame(0, $exitCode);
+        self::assertSame('120', $stdout);
+        self::assertSame('', $stderr);
+    }
 }
